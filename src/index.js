9f2611a11a8ad8e4569557bdb5ee2a5ab17a4134
Marcin Jaszczuk <martim0t0@gmail.com>
2022-03-10 20:55:35 -0600

    add Logo component, add logo picture, add logo css, configure corresponding imports. Add react-tilt after making sure tachyons is there.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import Tilt from "react-tilt";
 
 ReactDOM.render(
   <React.StrictMode>
